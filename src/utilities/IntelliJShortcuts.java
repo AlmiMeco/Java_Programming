f317d4fd7f39dc2078ed62837f3bd08cd3b2353a
Almi's PC <almimeci@gmail.com>
2022-12-13 19:24:12 -0600

    12/13/2022 ---> IntelliJ Shortcuts

---
+++ src/utilities/IntelliJShortcuts.java
@@ -0,0 +1,43 @@
+package utilities;
+
+public class IntelliJShortcuts {
+
+//    ctrl + shift + /  - - - >  (/**/)
+//    alt  +   /        - - - >  (copies last word)
+//    ctrl + space      - - - >  (shows you possible auto-fills)
+//    ctrl + arrow key  - - - >  (surf thru sentences one word at a time)
+//   shift + shift      - - - >  (pop up window with recent Java classes)
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//   select classes + right clck + diagram ---> (creates a diagram to show relationships amongst inherited classes)
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+}
